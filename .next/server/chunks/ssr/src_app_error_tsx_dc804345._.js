5de12d476a534d0cc560d5bc9e604a6326d7a794
Vitalii Beiar <37002507+nightven@users.noreply.github.com>
2025-05-16 21:23:23 -0230

    init repo

---
+++ .next/server/chunks/ssr/src_app_error_tsx_dc804345._.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+"[project]/src/app/error.tsx [app-rsc] (ecmascript)": (function(__turbopack_context__) {
+
+var { g: global, __dirname, m: module, e: exports } = __turbopack_context__;
+{
+}}),
+
+};
+
+//# sourceMappingURL=src_app_error_tsx_dc804345._.js.map
