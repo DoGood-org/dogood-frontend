36c5e9f8442bdf94bf370dbcade3af6c7def385b
Alona Kuznietsova <acvetochka@gmail.com>
2025-05-20 16:29:32 +0200

    change intl request

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,12 @@
-import { useTranslations } from "next-intl";
-import React from "react";
+import { useTranslations } from 'next-intl';
+import React from 'react';
 
 export default function Home() {
-    const t = useTranslations('HomePage');
+  const t = useTranslations('common');
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
-        <h1>{t('title')}</h1>
+      <h1>{t('title')}</h1>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
   );
--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,16 +1,35 @@
-import {getRequestConfig} from 'next-intl/server';
-import {hasLocale} from 'next-intl';
-import {routing} from './routing';
+import { getRequestConfig } from 'next-intl/server';
+import { hasLocale } from 'next-intl';
+import { routing } from './routing';
 
-export default getRequestConfig(async ({requestLocale}) => {
+export default getRequestConfig(async ({ requestLocale }) => {
   // Typically corresponds to the `[locale]` segment
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
 
+  const namespaces = [
+    'common',
+    'header',
+    'hero',
+    'about',
+    'map',
+    'faq',
+    'footer',
+  ];
+
+  const messages = Object.fromEntries(
+    await Promise.all(
+      namespaces.map(async (ns) => {
+        const msgs = await import(`../../messages/${locale}/${ns}.json`);
+        return [ns, msgs.default];
+      })
+    )
+  );
+
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default
+    messages,
   };
 });
