139a1ae344ea27b8ca166c61481d5d3d3a9ec71f
Alona Kuznietsova <acvetochka@gmail.com>
2025-05-21 11:02:01 +0200

    Add images for about-section

---
+++ src/assets/images/about/import.ts
@@ -0,0 +1,15 @@
+import about from './about.png';
+import donate from './donate.png';
+import goodBot from './goodBot.png';
+import grants from './grants.png';
+import myDeeds from './myDeeds.png';
+import volunteer from './volunteer.png';
+
+export const images = {
+  'about.png': about,
+  'donate.png': donate,
+  'goodBot.png': goodBot,
+  'grants.png': grants,
+  'myDeeds.png': myDeeds,
+  'volunteer.png': volunteer,
+};
