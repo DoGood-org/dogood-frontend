d29d6ae6d1063ef98512ad76fce13ae2b5022392
Vitalii Beiar <37002507+nightven@users.noreply.github.com>
2025-05-27 20:23:19 -0230

    Update index.ts

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -16,7 +16,4 @@ export { default as Search } from './Search';
 export { default as Settings } from './Settings';
 export { default as MessageIcon } from './MessageIcon';
 export { default as NameIcon } from './NameIcon';
-export { default as Plus } from './Plus';
-export { default as RiInstagramLine } from './RiInstagramLine';
-export { default as RiTwitterXLine } from './RiTwitterXLine';
 export { default as TelIcon } from './TelIcon';
