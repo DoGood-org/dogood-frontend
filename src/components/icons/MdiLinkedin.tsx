7a0640c17b2bf94894661922f08059759d066e67
Alona Kuznietsova <acvetochka@gmail.com>
2025-05-30 01:43:33 +0200

    add theme-toggle

--- src/components/icons/MdiLinkedin.tsx
+++ src/components/icons/MdiLinkedin.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import type { SVGProps } from 'react';
-const SvgMdiLinkedin = (props: SVGProps<SVGSVGElement>): React.JSX.Element => (
+const SvgMdiLinkedin = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
@@ -10,7 +10,7 @@ const SvgMdiLinkedin = (props: SVGProps<SVGSVGElement>): React.JSX.Element => (
     {...props}
   >
     <path
-      fill="currentColor"
+      fill="#fff"
       d="M23.75 3.75a2.5 2.5 0 0 1 2.5 2.5v17.5a2.5 2.5 0 0 1-2.5 2.5H6.25a2.5 2.5 0 0 1-2.5-2.5V6.25a2.5 2.5 0 0 1 2.5-2.5zm-.625 19.375V16.5a4.075 4.075 0 0 0-4.075-4.075c-1.062 0-2.3.65-2.9 1.625v-1.388h-3.487v10.463h3.487v-6.163c0-.962.775-1.75 1.738-1.75a1.75 1.75 0 0 1 1.75 1.75v6.163zM8.6 10.7a2.1 2.1 0 0 0 2.1-2.1 2.107 2.107 0 0 0-2.1-2.112A2.11 2.11 0 0 0 6.488 8.6c0 1.162.95 2.1 2.112 2.1m1.738 12.425V12.663H6.875v10.462z"
     />
   </svg>
--- src/components/icons/RiTwitterXLine.tsx
+++ src/components/icons/RiTwitterXLine.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import type { SVGProps } from 'react';
-const SvgRiTwitterXLine = (
-  props: SVGProps<SVGSVGElement>
-): React.JSX.Element => (
+const SvgRiTwitterXLine = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
@@ -12,7 +10,7 @@ const SvgRiTwitterXLine = (
     {...props}
   >
     <path
-      fill="currentColor"
+      fill="#fff"
       d="m13.11 18.314 5.953 7.936h8.75L17.99 13.153l8.173-9.403H22.85l-6.396 7.358-5.517-7.358h-8.75l9.388 12.519L2.9 26.25h3.313zm7.203 5.436L7.188 6.25h2.5l13.125 17.5z"
     />
   </svg>
