e09404c06d5480876621a2e45e906ba5d8d470e6
Alona Kuznietsova <acvetochka@gmail.com>
2025-05-31 23:34:04 +0200

    clear code

--- scripts/fix-line-endings.mjs
+++ scripts/fix-line-endings.mjs
@@ -1,26 +1,3 @@
-// import fs from 'fs';
-// import path from 'path';
-
-// const exts = ['.js', '.ts', '.tsx', '.json', '.md'];
-
-// function walk(dir) {
-//   fs.readdirSync(dir).forEach((file) => {
-//     const fullPath = path.join(dir, file);
-//     const stat = fs.statSync(fullPath);
-
-//     if (stat.isDirectory()) {
-//       walk(fullPath);
-//     } else if (exts.includes(path.extname(fullPath))) {
-//       const content = fs.readFileSync(fullPath, 'utf8');
-//       const newContent = content.replace(/\r\n/g, '\n');
-//       fs.writeFileSync(fullPath, newContent, 'utf8');
-//     }
-//   });
-// }
-
-// walk('./');
-// console.log('✅ Line endings fixed to LF');
-
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
