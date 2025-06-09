eb689c74be5ee4d56c771fd1d46db7ad998cd5ee
Alona Kuznietsova <acvetochka@gmail.com>
2025-06-09 17:08:56 +0200

    change key

--- src/components/layout/header/SettingList.tsx
+++ src/components/layout/header/SettingList.tsx
@@ -15,7 +15,7 @@ export const SettingsList: React.FC<SettingsContentProps> = ({
   return (
     <ul className="flex flex-col gap-[25px]">
       {content.map(({ id, name, description }) => (
-        <li className="flex gap-10 justify-between" key={name}>
+        <li className="flex gap-10 justify-between" key={id}>
           <div className="w-[215px]">
             <h4 className="mb-[22px]">{name}</h4>
             <p className="text-p2-d text-text-gray">{description}</p>
