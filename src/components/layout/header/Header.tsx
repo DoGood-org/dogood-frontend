f52c61ea7ace2a14cae7d46a2150c27c3658882b
Alona Kuznietsova <acvetochka@gmail.com>
2025-06-12 23:54:42 +0200

    change container styles

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,22 +1,24 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from '@/assets/logo.png';
-import { Navigation } from './Navigation';
+import { Container, Navigation } from '@/components';
 
 export const Header: React.FC = () => {
   return (
-    <header className="z-[9990] my-container fixed mx-auto bg-layout-background  h-[104px] overflow-visible">
-      <div className="flex justify-between items-center py-4 h-[104px]">
-        <Link href="/">
-          <Image
-            src={logo}
-            alt="Logo DoGood"
-            className="w-[135px] h-[40px]"
-            priority
-          />
-        </Link>
-        <Navigation />
-      </div>
+    <header className="z-[9990] fixed mx-auto bg-layout-background w-full h-[104px] overflow-visible">
+      <Container>
+        <div className="flex justify-between items-center py-4 h-[104px]">
+          <Link href="/">
+            <Image
+              src={logo}
+              alt="Logo DoGood"
+              className="w-[135px] h-[40px]"
+              priority
+            />
+          </Link>
+          <Navigation />
+        </div>
+      </Container>
     </header>
   );
 };
