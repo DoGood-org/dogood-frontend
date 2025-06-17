247d5171d6d44d8a93e7c3d6b9deee0c0b53082a
Alona Kuznietsova <acvetochka@gmail.com>
2025-06-17 15:35:25 +0200

    add Section component

---
+++ src/components/ui/Section.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container } from './Container';
+import { SectionProps } from '@/types';
+import { cn } from '@/lib/utils';
+
+export const Section = ({
+  children,
+  className = '',
+  withContainer = true,
+}: SectionProps): React.JSX.Element => {
+  const content = withContainer ? <Container>{children}</Container> : children;
+  return (
+    <section
+      className={cn(
+        'py-[30px] xl:py-[50px] bg-background transition-color transition-background duration-800',
+        className
+      )}
+    >
+      {content}
+    </section>
+  );
+};
---
+++ src/types/sectionTypes.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export interface SectionProps {
+  children: React.ReactNode;
+  className?: string;
+  withContainer?: boolean; // default true
+}
