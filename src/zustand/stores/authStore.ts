92f2e444c62dc8e8134aaf16c0940340915bd885
Alona Kuznietsova <acvetochka@gmail.com>
2025-06-25 01:07:25 +0200

    add example data to user

--- src/zustand/stores/authStore.ts
+++ src/zustand/stores/authStore.ts
@@ -1,10 +1,17 @@
 import { AuthState } from '@/types/authType';
 import { create } from 'zustand';
 
+const userExample = {
+  id: '',
+  email: 'user@mail.com',
+  name: 'User Name',
+  avatarUrl: '',
+};
+
 export const authStore = create<AuthState>((set) => ({
   isLoggedIn: true,
   // user: null,
-  user: { id: '', email: 'user@mail.com', name: 'User Name', avatarUrl: '' },
+  user: userExample,
   login: (userData): void => set({ user: userData, isLoggedIn: true }),
   logout: (): void => set({ user: null, isLoggedIn: false }),
 }));
