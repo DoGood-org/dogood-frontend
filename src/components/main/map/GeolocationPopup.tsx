3b79c606497cf954208bcbbfa7b07cff76c928b7
AnnetaDe <anneta.liss@gmail.com>
2025-06-25 01:26:38 +0200

    feat: enhance map component with geolocation popup and scroll handling
    - Added AnimatedModalWrapper and Portal components for modal functionality.
    - Integrated geolocation popup with improved visibility handling based on map view.
    - Implemented ScrollAfterDelay to manage scroll behavior on the map.
    - Updated Container component to accept a ref for better integration.
    - Refactored Map component to utilize new modal and scroll features.
    - Cleaned up unused code and improved state management for selected locations.
    - Added npm and install packages to package.json for better dependency management.

--- src/components/main/map/GeolocationPopup.tsx
+++ src/components/main/map/GeolocationPopup.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { Button } from '@/components/ui/Button';
-import { Container } from '@/components/ui/Container';
 import { IGeolocationPopupProps } from '@/types/mapType';
 import { useTranslations } from 'next-intl';
 import React, { FC } from 'react';
@@ -12,27 +11,25 @@ export const GeolocationPopup: FC<IGeolocationPopupProps> = ({
   const t = useTranslations('map');
 
   return (
-    <Container className="fixed inset-0 flex items-center justify-center z-[1000]">
-      <div className="bg-background text-foreground p-4 rounded-lg shadow-lg max-w-md w-full mx-4">
-        <h3 className="text-base font-semibold mb-4">{t('popupTitle')}</h3>
-        <p className="mb-4">{t('popupDescription')}</p>
-        <div className="flex justify-end space-x-4">
-          <Button
-            variant="secondary"
-            onClick={declineGeolocation}
-            className="btn-expand-hover"
-          >
-            {t('noBtn')}
-          </Button>
-          <Button
-            variant="primary"
-            onClick={requestGeolocation}
-            className="btn-expand-hover text-base"
-          >
-            {t('yesBtn')}
-          </Button>
-        </div>
+    <div className="bg-background text-foreground p-4 rounded-lg shadow-lg max-w-md w-full mx-4">
+      <h3 className="text-base font-semibold mb-4">{t('popupTitle')}</h3>
+      <p className="mb-4">{t('popupDescription')}</p>
+      <div className="flex justify-end space-x-4">
+        <Button
+          variant="secondary"
+          onClick={declineGeolocation}
+          className="btn-expand-hover"
+        >
+          {t('noBtn')}
+        </Button>
+        <Button
+          variant="primary"
+          onClick={requestGeolocation}
+          className="btn-expand-hover text-base"
+        >
+          {t('yesBtn')}
+        </Button>
       </div>
-    </Container>
+    </div>
   );
 };
