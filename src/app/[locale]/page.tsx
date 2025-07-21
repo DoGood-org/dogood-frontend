ca7d03e21e14615995862857769370551ddf9d0e
xPhoenixUAx <pavloff12@gmail.com>
2025-07-21 04:20:00 +0300

    upd

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,26 @@
-import Loader from '@/components/ui/Loader';
+import {
+  AboutSection,
+  Donate,
+  Faq,
+  HeroSection,
+  HowItWorks,
+  MapSection,
+  NewsList,
+  ReviewsSection,
+} from '@/components';
 import React from 'react';
 
 export default function Home(): React.JSX.Element {
   return (
     <>
-      <Loader />
+      <HeroSection />
+      <AboutSection />
+      <HowItWorks />
+      <MapSection />
+      <Donate />
+      <ReviewsSection />
+      <NewsList />
+      <Faq />
     </>
   );
 }
