1033b15fe142ba53d96954b651779a9d49e1d86c
xPhoenixUAx <pavloff12@gmail.com>
2025-07-24 05:31:16 +0300

    last upd

--- src/components/ui/ContentLoader.tsx
+++ src/components/ui/ContentLoader.tsx
@@ -1,19 +1,25 @@
 'use client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 type TrailDotProps = {
   delay: string;
   opacity: number;
+  uniqueKey: number;
 };
 
-const TrailDot = ({ delay, opacity }: TrailDotProps): React.ReactElement => (
+const TrailDot = ({
+  delay,
+  opacity,
+  uniqueKey,
+}: TrailDotProps): React.ReactElement => (
   <div
+    key={uniqueKey + delay}
     className="absolute top-1/2 left-1/2"
     style={{
-      animation: 'orbit 1s linear infinite',
+      animation: 'orbit 2s linear',
       animationDelay: delay,
+      animationFillMode: 'forwards',
       transformOrigin: 'center',
-      transform: 'rotate(0deg) translate(-50%, -50%)',
     }}
   >
     <span
@@ -26,22 +32,50 @@ const TrailDot = ({ delay, opacity }: TrailDotProps): React.ReactElement => (
   </div>
 );
 
-const createTrail = (startDelay = 0): React.ReactElement[] =>
+const createTrail = (startDelay = 0, key: number): React.ReactElement[] =>
   Array.from({ length: 6 }).map((_, i) => {
     const delay = `${(i * 0.1 + startDelay).toFixed(2)}s`;
     const opacity = 1 - i * 0.15;
     return (
-      <TrailDot key={`${startDelay}-${i}`} delay={delay} opacity={opacity} />
+      <TrailDot
+        key={`${key}-${startDelay}-${i}`}
+        delay={delay}
+        opacity={opacity}
+        uniqueKey={key}
+      />
     );
   });
 
-const ContentLoader: React.FC = () => (
-  <div className="flex items-center justify-center w-screen h-screen">
-    <div className="relative w-16 h-16">
-      {createTrail(0)}
-      {createTrail(0.5)}
+const ContentLoader: React.FC = () => {
+  const [animationKey, setAnimationKey] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAnimationKey((prev) => prev + 1);
+    }, 3000); // 2s анімація + 1s пауза
+    return (): void => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center w-screen h-screen">
+      <div className="relative w-16 h-16">
+        {createTrail(0, animationKey)}
+        <div
+          style={{
+            position: 'absolute',
+            top: '63%',
+            left: '50%',
+            width: '100%',
+            height: '100%',
+            transform: 'translate(-50%, -50%) rotate(180deg)',
+            transformOrigin: 'center',
+          }}
+        >
+          {createTrail(0, animationKey)}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ContentLoader;
