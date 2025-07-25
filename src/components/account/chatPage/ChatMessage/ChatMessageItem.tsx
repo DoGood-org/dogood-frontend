208abde82e7fcacd96e6c92b7fa1b47e83bf84de
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2025-07-25 22:33:38 +0300

    update styles Chat, ChatMessageItem

--- src/components/account/chatPage/ChatMessage/ChatMessageItem.tsx
+++ src/components/account/chatPage/ChatMessage/ChatMessageItem.tsx
@@ -6,13 +6,13 @@ export const ChatMessageItem: React.FC<MessageItemProps> = ({ message }) => {
   return (
     <li
       className={cn(
-        'flex items-start gap-4 p-5 relative rounded-sm',
+        'flex items-start gap-3 py-3 px-6 w-[318px] relative rounded-sm md:w-[608px]',
         message.isCurrentUser
           ? 'ml-auto bg-[#3D8940] text-[#010101] dark:text-white'
           : 'mr-auto bg-tag text-white dark:bg-[#DCDCDC] dark:text-[#010101]'
       )}
     >
-      <div className={`${message.isCurrentUser ? 'order-2' : 'order-1'}`}>
+      <div className={message.isCurrentUser ? 'order-2' : 'order-1'}>
         <Image
           src={message.avatar}
           alt={`${message.name} avatar`}
@@ -21,15 +21,14 @@ export const ChatMessageItem: React.FC<MessageItemProps> = ({ message }) => {
           className="w-[42px] h-[42px] rounded-full object-cover bg-white shrink-0"
         />
       </div>
-
       <div
         className={cn(
-          'flex flex-col justify-between w-[210px] md:w-[506px]',
+          'flex flex-col justify-between flex-1 max-w-full',
           message.isCurrentUser ? 'order-1' : 'order-2'
         )}
       >
-        <p className="text-base">{message.content}</p>
-        <span className="text-xs self-end mt-3">
+        <p className="text-base break-words">{message.content}</p>
+        <span className="text-xs self-end mt-2 opacity-70">
           {new Date(message.createdAt).toLocaleDateString(undefined, {
             day: '2-digit',
             month: '2-digit',
--- src/components/account/chatPage/ChatMessage/ChatMessagesList.tsx
+++ src/components/account/chatPage/ChatMessage/ChatMessagesList.tsx
@@ -7,7 +7,9 @@ export const ChatMessageList: React.FC<MessagesListProps> = ({ messages }) => {
   return (
     <div className="flex flex-col flex-grow min-w-0 w-full">
       {messages.length === 0 ? (
-        <p className="text-black text-center mt-6">Немає повідомлень</p>
+        <p className="w-[318px] text-black text-center mt-6 md:w-[608px]">
+          Немає повідомлень
+        </p>
       ) : (
         <ul className="flex flex-col gap-6 mt-6">
           {messages.map((message) => (
