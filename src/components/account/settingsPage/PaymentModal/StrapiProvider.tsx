c7e309b6ba9d072451855d350b9495319b3a4cd0
Alona Kuznietsova <acvetochka@gmail.com>
2025-08-01 12:13:35 +0200

    add Strapi provider

--- src/components/account/settingsPage/PaymentModal/StrapiProvider.tsx
+++ src/components/account/settingsPage/PaymentModal/StrapiProvider.tsx
@@ -4,20 +4,25 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { JSX, ReactNode } from 'react';
 
-const stripePromise = loadStripe(
-  'pk_test_51RqHtID06DK4hMNdwtG380NHOLLZ2oNzta2D2po6GwZE9NIgV3Dj7v0W8SSAQgn1nQC109RXd6y0F2dM54k1hokh007S25f5bh'
-); // Замінити на live пізніше
+const stripePublicKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!;
+
+const stripePromise = loadStripe(stripePublicKey); // Замінити на live пізніше
 
 type Theme = 'flat' | 'stripe' | 'night' | undefined;
 
 const appearance = {
   theme: 'flat' as Theme,
-  // variables: {
-  //   colorBackground: 'var(--input-bg)',
-  //   colorText: 'var(--text)',
-  //   colorPrimary: 'var(--primary)',
-  //   spacingUnit: '6px',
-  // },
+  wallets: {
+    // applePay: 'never',
+    // googlePay: 'never',
+    link: 'never',
+  },
+  variables: {
+    colorBackground: 'var(--white)',
+    colorText: '#0D0D0D',
+    // colorPrimary: 'var(--primary)',
+    spacingUnit: '6px',
+  },
 };
 
 export const StripeProvider = ({
