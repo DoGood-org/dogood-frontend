d5c030087df7b9a0abb9e6fc3cac5e3716f0e033
Alona Kuznietsova <acvetochka@gmail.com>
2025-08-01 18:59:58 +0200

    change reviews-section-styles

--- src/components/main/reviews/ReviewCard.tsx
+++ src/components/main/reviews/ReviewCard.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils';
 import { ReviewCardProps } from '@/types';
 import { JSX } from 'react';
 
@@ -8,10 +9,13 @@ export const ReviewCard = ({
 }: ReviewCardProps): JSX.Element => {
   return (
     <div
-      className={`rounded-lg p-[32px] shadow-[0px_4px_4px_0px_#00000040] 
-                    ${index % 2 ? 'bg-text-help text-white md:bg-review-reverse md:text-foreground lg:bg-text-help lg:text-white' : 'bg-background text-foreground md:bg-review-bg md:text-white lg:bg-black lg:text-white'} 
-                    ${className}
-                    `}
+      className={cn(
+        'rounded-lg p-[32px] shadow-[0px_4px_4px_0px_#00000040] ',
+        index % 2
+          ? 'bg-review-bg text-white'
+          : 'bg-background text-foreground border border-border-hover',
+        className
+      )}
     >
       <p className="text-base">{review.review}</p>
       <p className="mt-[10px] font-semibold text-base">{review.author}</p>
