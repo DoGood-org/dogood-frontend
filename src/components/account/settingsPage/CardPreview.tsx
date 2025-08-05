30c8ec7aa072e3aa738e13746c7d875154bf7fac
Alona Kuznietsova <acvetochka@gmail.com>
2025-08-05 23:49:42 +0200

    change CardPreview

--- src/components/account/settingsPage/CardPreview.tsx
+++ src/components/account/settingsPage/CardPreview.tsx
@@ -3,7 +3,7 @@
 import { fetchUserCards } from '@/lib/api';
 import { cardPreviewStore } from '@/zustand/stores/cardPreviewStore';
 import { JSX, useEffect, useState } from 'react';
-import { cardIcons } from '@/components';
+import { Button, cardIcons } from '@/components';
 import { CardData } from '@/types';
 import { useTranslations } from 'next-intl';
 
@@ -54,40 +54,49 @@ export const CardPreview = ({
         const Icon = cardIcons[card?.brand ?? ''] ?? cardIcons.default;
         const formattedExpiry = `${String(card.exp_month).padStart(2, '0')}/${String(card.exp_year).slice(-2)}`;
         return (
-          <div
+          <li
             key={card.paymentMethodId}
-            className="border p-4 rounded-md bg-gray-50 text-sm mt-4"
+            className="border p-3 rounded-lg bg-gray-50 text-base w-[225px] md:min-w-[268px] lg:min-w-[284px] flex flex-col justify-between"
           >
-            <div className="flex gap-3">
-              <Icon className="w-9 h-[26px] rounded-sm" />
+            <div className="flex gap-3 text-black">
+              <div className="w-9 h-[26px] flex justify-center items-center">
+                <Icon className="size-9 w-9 h-[26px] rounded-sm" />
+              </div>
               <div>
-                <div className="flex gap-1">
+                <div className="flex gap-1 text-black">
                   <p className="capitalize">{card.brand}</p>
                   <span>****{card.last4}</span>
                 </div>
-                <p className="opacity-[0.5] mt-[6px]">
+                <p className="opacity-[0.5] mt-[6px] text-wrap md:text-nowrap md:text-sm">
                   {t('cardPeriod')}: {formattedExpiry}
                 </p>
               </div>
             </div>
-            <div className="mt-2 flex gap-4">
-              <button
+            <div className="mt-2 flex justify-between gap-4">
+              <Button
+                variant="ghost"
+                // size="xl"
+                className="text-black text-base hover:text-error p-0 h-6"
+                type="button"
+                onClick={() => deleteOnClick(card)}
+                // className="text-red-600 hover:underline text-sm"
+              >
+                {t('delete')}
+              </Button>
+              <Button
+                variant="ghost"
+                // size="xl"
+                className="text-black text-base  hover:text-btn-hover p-0 h-6"
+                type="button"
                 onClick={() => {
                   setEditingId(card.paymentMethodId);
                   setOpen(true);
                 }}
-                className="text-blue-600 hover:underline text-sm"
               >
                 {t('edit')}
-              </button>
-              <button
-                onClick={() => deleteOnClick(card)}
-                className="text-red-600 hover:underline text-sm"
-              >
-                {t('delete')}
-              </button>
+              </Button>
             </div>
-          </div>
+          </li>
         );
       })}
     </>
