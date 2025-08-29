a4f5a4dae64bcfa7986253f35e72014c0f4771b5
Vitalii Beiar <37002507+nightven@users.noreply.github.com>
2025-08-29 11:43:44 -0230

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,12 +12,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**',
-        pathname: '/**',
-      },
+      }
     ],
   },
 };
