c8c0ed8b83ee9d7f59ea751f3146d3c38a7af5cc
Alona Kuznietsova <acvetochka@gmail.com>
2025-09-05 21:54:39 +0200

    update UserNoDescription for using PublicUserPage

--- src/components/account/accountPage/UserNoDescription.tsx
+++ src/components/account/accountPage/UserNoDescription.tsx
@@ -1,26 +1,37 @@
+'use client';
+
 import { LinkWithArrow } from '@/components';
-import { navigationStore } from '@/zustand/stores/navigationStore';
-import { useTranslations } from 'next-intl';
+import { useRouteMatch } from '@/hooks/useRouteMatch';
+import { useLocale, useTranslations } from 'next-intl';
 import { JSX } from 'react';
 
 export const UserNoDescription = (): JSX.Element => {
   const t = useTranslations('account');
-  const { setCurrentPage } = navigationStore();
-
-  const handleOnClick = (e: React.MouseEvent<HTMLAnchorElement>): void => {
-    e.preventDefault();
-    setCurrentPage('Settings');
-  };
+  const locale = useLocale();
+  const isAccountPage = useRouteMatch('/account');
+  const isPublicProfilePage = useRouteMatch('/profile');
 
   return (
     <>
-      <p className="mt-6 text-base">{t('noDescription')}</p>
-      <LinkWithArrow
-        href=""
-        onClick={handleOnClick}
-        text={t('noDescriptionButton')}
-        className="mt-5"
-      />
+      {isAccountPage && (
+        <>
+          <p className="mt-6 text-base">{t('noDescription')}</p>
+          <LinkWithArrow
+            href={`/${locale}/account/settings`}
+            text={t('noDescriptionButton')}
+            className="mt-5"
+          />
+        </>
+      )}
+
+      {isPublicProfilePage && (
+        <>
+          <h3 className="mt-6 text-base">{t('description')}</h3>
+          <p className="mt-6 text-base font-semibold">
+            {t('noDescriptionPublic')}
+          </p>
+        </>
+      )}
     </>
   );
 };
