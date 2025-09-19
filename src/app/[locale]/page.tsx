5c05d3a8c8078cf3afb9012d1719fc9eae20d8e3
xPhoenixUAx <pavloff12@gmail.com>
2025-09-19 13:20:11 +0300

    +

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,26 +1,28 @@
-import {
-  AboutSection,
-  Donate,
-  Faq,
-  HeroSection,
-  HowItWorks,
-  MapSection,
-  NewsList,
-  ReviewsSection,
-} from '@/components';
+// import {
+//   AboutSection,
+//   Donate,
+//   Faq,
+//   HeroSection,
+//   HowItWorks,
+//   MapSection,
+//   NewsList,
+//   ReviewsSection,
+// } from '@/components';
+import Cookies from '@/components/main/cookies/Cookies';
 import React from 'react';
 
 export default function Home(): React.JSX.Element {
   return (
     <>
-      <HeroSection />
+      <Cookies />
+      {/* <HeroSection />
       <AboutSection />
       <HowItWorks />
       <MapSection />
       <Donate />
       <ReviewsSection />
       <NewsList />
-      <Faq />
+      <Faq /> */}
     </>
   );
 }
--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -22,6 +22,7 @@ export async function loadMessages(locale: string): Promise<Messages> {
     'tasks',
     'chat',
     'card',
+    'cookies',
   ];
 
   const messages = Object.fromEntries(
