1765fb2c5c2b308cbc1421f173a89abd642af8a8
xPhoenixUAx <pavloff12@gmail.com>
2025-09-23 23:37:19 +0300

    rendering

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,28 +1,26 @@
-// import {
-//   AboutSection,
-//   Donate,
-//   Faq,
-//   HeroSection,
-//   HowItWorks,
-//   MapSection,
-//   NewsList,
-//   ReviewsSection,
-// } from '@/components';
-import Cookies from '@/components/main/cookies/Cookies';
+import {
+  AboutSection,
+  Donate,
+  Faq,
+  HeroSection,
+  HowItWorks,
+  MapSection,
+  NewsList,
+  ReviewsSection,
+} from '@/components';
 import React from 'react';
 
 export default function Home(): React.JSX.Element {
   return (
     <>
-      <Cookies />
-      {/* <HeroSection />
+      <HeroSection />
       <AboutSection />
       <HowItWorks />
       <MapSection />
       <Donate />
       <ReviewsSection />
       <NewsList />
-      <Faq /> */}
+      <Faq />
     </>
   );
 }
