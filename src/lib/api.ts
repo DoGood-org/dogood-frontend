bc1872cd5fb54dbd6ddc7da3b673fb57c50af9b1
AnnDe <anneta.liss@gmail.com>
2025-09-23 11:13:19 +0200

    refactor(api): simplify base URL logic and remove unused getBaseURL function

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,32 +1,26 @@
 import axios from 'axios';
 
-// const getBaseURL = (): string => {
-//   if (typeof window !== 'undefined') return '/api'; // client → Next rewrite
-//   if (process.env.API_URL_INTERNAL) return process.env.API_URL_INTERNAL;
-//   if (process.env.BASE_URL) return process.env.BASE_URL;
-
-//   return `${process.env.API_URL}/api`;
-// };
-const getBaseURL = (): string => {
-  if (typeof window !== 'undefined') return '';
+const isBrowser = typeof window !== 'undefined';
+const FRONT_END_ORIGIN = (): string => {
   const appLocal = process.env.API_URL_INTERNAL;
   const appPublic = process.env.BASE_URL;
+
   let app = '';
   if (process.env.NODE_ENV === 'production') {
     if (appPublic) app = appPublic;
-
-    if (!app)
-      throw new Error('No BASE_URL or API_URL_INTERNAL defined in production');
-    return app;
+  } else {
+    app = appLocal || 'http://localhost:3000';
   }
-  app = appLocal || 'http://localhost:3000';
 
   return app;
 };
 
-const ORIGIN = getBaseURL();
-const guestBase = ORIGIN ? `${ORIGIN}/` : '/api'; // public routes (login/logout/refresh/signup)
-const authBase = ORIGIN ? `${ORIGIN}/api/proxy` : '/api/proxy'; // protected routes via server proxy
+const BACKEND = isBrowser
+  ? process.env.NEXT_PUBLIC_API_URL
+  : FRONT_END_ORIGIN();
+
+const guestBase = BACKEND ? `${BACKEND}/` : '/api'; // public routes (login/logout/refresh/signup)
+const authBase = BACKEND ? `${BACKEND}/api/proxy` : '/api/proxy'; // protected routes via server proxy
 
 const apiAuth = axios.create({
   baseURL: authBase,
@@ -35,7 +29,7 @@ const apiAuth = axios.create({
 
 const apiGuest = axios.create({
   baseURL: guestBase,
-  withCredentials: true,
+  withCredentials: false,
 });
 
 const api = {
