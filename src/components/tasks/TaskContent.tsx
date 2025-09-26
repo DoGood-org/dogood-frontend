c820d0ec2b8dfcbfa8df8b399bf508259643d887
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2025-09-26 14:22:48 +0300

    feat(tasks): Implement TaskList and TaskContent data flow

--- src/components/tasks/TaskContent.tsx
+++ src/components/tasks/TaskContent.tsx
@@ -12,6 +12,7 @@ interface TaskContentProps {
 export const TaskContent: React.FC<TaskContentProps> = ({ slug }) => {
   const tasks = generateTasks(49.8429, 24.0316);
   const detailedTasks: ITaskDetails[] = generateMockTasks(tasks);
+  console.log('detailedTasks', detailedTasks);
 
   const task = detailedTasks.find((t) => t.id === slug) ?? detailedTasks[0];
   return (
