f0e5aef1e28d9f46e15b22f9e427ee696180e33d
Oleg_Makoid <oleh_makoid3007@tntu.edu.ua>
2025-11-05 17:51:06 +0200

    Added PostGIS

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone https://github.com/DoGood-org/dogood-backend /backend/  \
+ && cd /backend && npm install \ 
+ && npx prisma generate \
+ && rm -rf /var/lib/apt/lists/* \
+ && npm cache clean --force
+
+
+ npx prisma migrate dev
