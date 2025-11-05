3cb5d8d5c87a5f6427b79d7a92294bd1d8b10b84
Oleg_Makoid <oleh_makoid3007@tntu.edu.ua>
2025-11-05 18:22:41 +0200

    Fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-git clone https://github.com/DoGood-org/dogood-backend /backend/  \
- && cd /backend && npm install \ 
- && npx prisma generate \
- && rm -rf /var/lib/apt/lists/* \
- && npm cache clean --force
-
-
- npx prisma migrate dev
+git clone https://github.com/DoGood-org/dogood-backend /backend && cd /backend && npm install && npx prisma generate && rm -rf /var/lib/apt/lists/* && npm cache clean --force && npx prisma migrate dev
