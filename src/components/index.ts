03fc48812af040cd5f1da149316c91397148f137
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2025-11-26 16:25:10 +0200

    delete NewsScrollList update SwiperList and clean imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -95,7 +95,6 @@ export * from './main/hero/HeroSocialLink';
 export * from './main/howItWorks/HowItWorks';
 export * from './main/news/NewsItem';
 export * from './main/news/NewsList';
-export * from './main/news/NewsScrollList';
 export * from './main/news/NewsSlideTablet';
 export * from './main/news/SwiperList';
 export * from './main/news/SwiperNavButton';
--- src/components/main/news/NewsList.tsx
+++ src/components/main/news/NewsList.tsx
@@ -1,16 +1,15 @@
 import React, { JSX } from 'react';
 
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-// import { SwiperList } from '@/components/main/news/SwiperList';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { SwiperList } from '@/components/main/news/SwiperList';
 
 import { LinkWithArrow } from '@/components/ui/LinkWithArrow';
 import { Section } from '@/components/ui/Section';
 import { getLocale, getTranslations } from 'next-intl/server';
 import { fetchNews } from '@/facades/newsFacade';
 import { Tlocale } from '@/types';
-import { NewsScrollList } from '@/components';
 
 export const NewsList = async (): Promise<JSX.Element> => {
   const locale = (await getLocale()) as Tlocale;
@@ -36,7 +35,15 @@ export const NewsList = async (): Promise<JSX.Element> => {
         {t('newsListMain.title')}
       </h2>
 
-      <NewsScrollList newsItems={newsItems} />
+      <SwiperList
+        newsItems={newsItems}
+        swiperContainerClass="h-[425px] my-10"
+        prevClass="prevNews"
+        nextClass="nextNews"
+        paginationClass="news-pagination"
+        bulletClass="news-pagination-bullet"
+        bulletActiveClass="news-pagination-bullet-active"
+      />
 
       <div className="mt-4 flex justify-end">
         <LinkWithArrow href="/news" text={t('newsListMain.seeAll')} />
