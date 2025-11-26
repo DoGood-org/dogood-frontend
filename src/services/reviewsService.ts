c585f98a00c434386389be93849a304bfc531b8e
Oksana-Tnt <oksanatantsiura@gmail.com>
2025-11-26 14:07:56 +0100

    Fix reviews service

--- src/services/reviewsService.ts
+++ src/services/reviewsService.ts
@@ -1,21 +1,12 @@
-import { fetchFromApi } from '@/lib/apiFetcher';
-import { ReviewsFormData, ReviewsResponse } from '@/types/reviews';
+import { fetchFromApi, FetchResult } from '@/lib/apiFetcher';
+import { ReviewsFormData } from '@/types/reviews';
 
 export const sendReview = async (
   formData: ReviewsFormData
-): Promise<ReviewsResponse> => {
-  const response = await fetchFromApi<ReviewsResponse>('/reviews/users', {
+): Promise<FetchResult<ReviewsFormData>> => {
+  return await fetchFromApi<ReviewsFormData>('/reviews/users', {
     method: 'POST',
     data: formData,
     auth: true,
   });
-  if (response.ok) {
-    return response.data;
-  } else {
-    throw {
-      status: 'error',
-      message: response.errorMessage,
-      data: null,
-    };
-  }
 };
