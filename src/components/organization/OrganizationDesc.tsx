8e61138f3b093f2ae2c25b0097beff23bfb846c7
Alona Kuznietsova <acvetochka@gmail.com>
2025-12-01 21:38:59 +0100

    add adaptive to decription into organization-layout

--- src/components/organization/OrganizationDesc.tsx
+++ src/components/organization/OrganizationDesc.tsx
@@ -24,17 +24,22 @@ export const OrganizationDesc = ({
   const adminRole = isAdminOrModerator(userRole);
 
   return (
-    <div className="flex flex-col md:flex-row gap-11 lg:gap-20">
-      <Image
-        src={avatar ? avatar : '/account/avatar.png'}
-        alt={`${name} avatar`}
-        width={353}
-        height={353}
-        className="w-[353px] h-[353px] object-cover md:w-[192px] md:h-[192px] lg:w-[336px] lg:h-[336px] rounded-[10px]"
-      />
-      <div className="w-full">
+    <div className="flex flex-col md:grid md:grid-cols-[192px_425px] md:grid-rows-[192px_auto] md:gap-x-32px gap-11 lg:grid-cols-[336px_auto] lg:grid-rows-[auto_auto] lg:gap-x-20">
+      {/* IMAGE */}
+      <div className="lg:row-span-full">
+        <Image
+          src={avatar ? avatar : '/account/avatar.png'}
+          alt={`${name} avatar`}
+          width={353}
+          height={353}
+          className="w-[353px] h-[353px] object-cover md:w-[192px] md:h-[192px] lg:w-[336px] lg:h-[336px] rounded-[10px]"
+        />
+      </div>
+
+      {/* TEXT BLOCK (title, subtitle, contact-list) */}
+      <div className="w-full lg:col-start-2 lg:row-start-1 ">
         <div className="flex justify-between">
-          <h2 className="text-h2-m md:text-h2 lg:text-h2-d">{name}</h2>
+          <h2 className="text-h2-m lg:text-h2-d">{name}</h2>
           <Report role={userRole} />
         </div>
         <p className="text-base lg:text-h3 mt-3 text-text-help lg:font-normal capitalize">
@@ -56,15 +61,23 @@ export const OrganizationDesc = ({
             </p>
           )}
         </div>
-        {description ? (
-          <>
-            <h3 className="mt-6">{t('description')}</h3>
-            <p className="whitespace-pre-line mt-6 text-base">{description}</p>
-          </>
-        ) : (
-          <UserNoDescription />
-        )}
-        <div className="flex gap-3 justify-end mt-10">
+      </div>
+
+      <div className="flex flex-col md:flex-row md:row-start-2 md:col-span-full md:gap-10 lg:col-start-2 lg:flex-col">
+        {/* DESCRIPTION */}
+        <div className="flex flex-col shrink-0 md:w-[421px] lg:flex-col">
+          {description ? (
+            <>
+              <h3 className="mt-6">{t('description')}</h3>
+              <p className="whitespace-pre-line text-base">{description}</p>
+            </>
+          ) : (
+            <UserNoDescription />
+          )}
+        </div>
+
+        {/* BUTTONS */}
+        <div className="flex gap-3 md:w-[187px] md:flex-col lg:flex-row lg:w-full justify-end mt-10">
           {!adminRole && (
             <Button asChild variant="secondary" className="">
               <Link href={`/${locale}/account/chat`}>
@@ -78,6 +91,7 @@ export const OrganizationDesc = ({
           )}
         </div>
       </div>
+      {/* </div> */}
     </div>
   );
 };
