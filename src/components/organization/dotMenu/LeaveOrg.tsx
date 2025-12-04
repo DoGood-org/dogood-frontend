c6c331e60369f53aec4ab64707090c48351ce2db
Alona Kuznietsova <acvetochka@gmail.com>
2025-12-04 20:37:03 +0100

    add LeaveModal to Organization-description

--- src/components/organization/dotMenu/LeaveOrg.tsx
+++ src/components/organization/dotMenu/LeaveOrg.tsx
@@ -3,13 +3,27 @@ import { Button } from '@/components/ui/Button';
 import { useTranslations } from 'next-intl';
 import { JSX } from 'react';
 
-export const LeaveOrg = (): JSX.Element => {
+type LeaveOrgProps = {
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const LeaveOrg = ({
+  setIsModalOpen,
+  setIsOpen,
+}: LeaveOrgProps): JSX.Element => {
   const t = useTranslations('organization');
+
+  const handleOnClick = (): void => {
+    setIsOpen(false);
+    setIsModalOpen(true);
+  };
 
   return (
     <li className="text-nowrap">
       <Button
         variant="ghost"
+        onClick={handleOnClick}
         className="p-3 h-[48px] border-0 flex items-center justify-between w-full 
                 hover:text-btn-hover active:text-btn-active cursor-pointer text-foreground transition flex gap-4 justify-start"
       >
