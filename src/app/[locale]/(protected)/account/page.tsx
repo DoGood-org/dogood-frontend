e7d0bd3ad5f031d943aa61a89b31450f80eb5376
Alona Kuznietsova <acvetochka@gmail.com>
2025-12-15 23:43:50 +0100

    add prop to Account-component

--- src/app/[locale]/(protected)/account/page.tsx
+++ src/app/[locale]/(protected)/account/page.tsx
@@ -1,8 +1,9 @@
 import { Account } from '@/components/account/accountPage/Account';
 import React from 'react';
+import { mockUser } from '@/data/mockUser';
 
 const AccountPage: React.FC = () => {
-  return <Account />;
+  return <Account user={mockUser} />;
 };
 
 export default AccountPage;
