b1ed45940cfc84cad8e8617d6ff7ccb4cdf01d08
Alona Kuznietsova <acvetochka@gmail.com>
2025-12-19 16:03:24 +0100

    update organization-profile-page using facade

--- src/app/[locale]/organization/[id]/profile/page.tsx
+++ src/app/[locale]/organization/[id]/profile/page.tsx
@@ -1,22 +1,14 @@
 import { notFound } from 'next/navigation';
-import { getOrganizationById } from '@/services/organizationService';
-import { OrganizationDetailedProps, Tlocale } from '@/types';
+import { Tlocale } from '@/types';
 import { Metadata } from 'next';
-import React, { cache, JSX } from 'react';
+import React, { JSX } from 'react';
 import { OrganizationProfile } from '@/components/organization/profilePage/OrganizationProfile';
+import { fetchOrganizationById } from '@/facades/organizationFacade';
 
 interface Props {
   params: Promise<{ id: string; locale: Tlocale }>;
 }
 
-const fetchOrganizationById = cache(
-  async (id: string): Promise<OrganizationDetailedProps | null> => {
-    const organization = await getOrganizationById(id);
-
-    if (!organization) notFound();
-    return organization;
-  }
-);
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { id } = await params;
 
