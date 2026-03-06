f943f71bece6b9e2eb28264397aff19c6bb76680
Alona Kuznietsova <acvetochka@gmail.com>
2026-03-06 13:39:54 +0100

    create org permissions

--- src/lib/getUserRole.ts
+++ src/lib/getUserRole.ts
@@ -1,24 +1,32 @@
-import { OrganizationRole, UserOrganization } from '@/types';
+import { Role, UserOrganization } from '@/types';
 import { authStore } from '@/zustand/stores/authStore';
 
-export type Role = OrganizationRole | 'USER';
+// export const getUserRole = (members: UserOrganization[]): Role => {
+//   const currentUser = authStore((s) => s.user);
+//   const noRole = 'USER';
+
+//   if (!currentUser) return noRole;
+
+//   const userWithRole = members.filter(
+//     ({ userId }) => userId === currentUser.id
+//   );
+
+//   if (!userWithRole || !userWithRole.length) {
+//     return noRole;
+//   } else {
+//     const userRole = userWithRole[0].role;
+//     return userRole;
+//   }
+// };
+// export type PermissionRole = Role | 'GUEST';
 
 export const getUserRole = (members: UserOrganization[]): Role => {
   const currentUser = authStore((s) => s.user);
-  const noRole = 'USER';
+  if (!currentUser) return 'GUEST';
 
-  if (!currentUser) return noRole;
+  const member = members.find(({ userId }) => userId === currentUser.id);
 
-  const userWithRole = members.filter(
-    ({ userId }) => userId === currentUser.id
-  );
-
-  if (!userWithRole || !userWithRole.length) {
-    return noRole;
-  } else {
-    const userRole = userWithRole[0].role;
-    return userRole;
-  }
+  return member?.role ?? 'USER';
 };
 
 export const isAdminOrModerator = (role: Role): boolean => {
