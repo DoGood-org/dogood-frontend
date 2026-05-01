90e33007dbc7f6fd235425aae63d23e2a932a97c
AtamanchukM <maksumatmn@gmail.com>
2026-05-01 08:16:14 -0700

    add reviews block

--- src/app/[locale]/donate/page.tsx
+++ src/app/[locale]/donate/page.tsx
@@ -2,15 +2,21 @@ import DonateHero from '@/components/donate/DonateHero';
 import DonateSteps from '@/components/donate/DonateSteps';
 import MetricsSection from '@/components/donate/MetricsSection';
 import WhySupportMatters from '@/components/donate/WhySupportMatters';
+import { WhatPeopleSay } from '@/components/grantsPage/WhatPeopleSay';
+import { SupportMission } from '@/components/ui/SupportMission';
+import { useTranslations } from 'next-intl';
 import React from 'react';
 
 const DonatePage: React.FC = () => {
+  const  t = useTranslations('donatePage');
   return (
     <>
       <DonateHero />
       <DonateSteps />
+      <SupportMission href="#" description={t('supportMission.description')} />
       <MetricsSection />
       <WhySupportMatters />
+      <WhatPeopleSay />
     </>
   );
 };
