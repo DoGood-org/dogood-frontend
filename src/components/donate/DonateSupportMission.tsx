a4d3bc6cdeec1c9e73934d0e6076b85eda84bbdf
AtamanchukM <maksumatmn@gmail.com>
2026-05-11 12:47:47 +0300

    add type

--- src/components/donate/DonateSupportMission.tsx
+++ src/components/donate/DonateSupportMission.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import React, { JSX } from 'react';
 import { useTranslations } from 'next-intl';
 import { SupportMission } from '@/components/ui/SupportMission';
 import { donationModalStore } from '@/zustand/stores/donationModalStore';
 
-export function DonateSupportMission() {
+export function DonateSupportMission(): JSX.Element {
   const t = useTranslations('donatePage');
   const open = donationModalStore((s) => s.open);
 
