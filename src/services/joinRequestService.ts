f4d8415a50c6cb201bd62bd6afe0f593c39ce536
Alona Kuznietsova <acvetochka@gmail.com>
2026-06-01 21:47:18 +0200

    create joinRequestService

--- src/services/joinRequestService.ts
+++ src/services/joinRequestService.ts
@@ -1,27 +1,46 @@
 import { fetchFromApi, FetchResult } from '@/lib/api/apiFetcher';
 import { apiRoutes } from '@/lib/server/apiRoutes';
-
-export type DirectionRequest = 'FROM_USER' | 'FROM_ORGANIZATION';
-export type JoinRequestStatus =
-  | 'PENDING'
-  | 'ACCEPTED'
-  | 'REJECTED'
-  | 'CANCELLED';
-
-export interface JoinRequestApiData {
-  senderId: string | number;
-  receiverOrganizationId: string;
-  receiverUserId?: string;
-  direction: DirectionRequest;
-  status?: JoinRequestStatus;
-}
+import {
+  ICreateJoinRequestResponse,
+  IJoinRequestResponse,
+  IJoinRequests,
+  IJoinRequestApiData,
+} from '@/types/joinRequest.type';
 
 export const createJoinRequest = async (
-  data: JoinRequestApiData
-): Promise<FetchResult<JoinRequestApiData>> => {
-  return fetchFromApi<JoinRequestApiData>(apiRoutes.joinRequests.create, {
-    method: 'POST',
-    data,
-    auth: true,
-  });
+  data: IJoinRequestApiData
+): Promise<FetchResult<IJoinRequests>> => {
+  const result = await fetchFromApi<ICreateJoinRequestResponse>(
+    apiRoutes.joinRequests.create,
+    {
+      method: 'POST',
+      data,
+      auth: true,
+    }
+  );
+
+  if (!result.ok) return result;
+
+  return {
+    ok: true,
+    data: result.data.data.joinRequest,
+  };
+};
+
+export const getJoinRequests = async (
+  id: string
+): Promise<FetchResult<IJoinRequests[]>> => {
+  const result = await fetchFromApi<IJoinRequestResponse>(
+    apiRoutes.joinRequests.getJoinRequests(id),
+    { method: 'GET', auth: true }
+  );
+
+  console.log('JOIN REQUESTS RESULT:', result);
+
+  if (!result.ok) return result;
+
+  return {
+    ok: true,
+    data: result.data?.data?.joinRequests ?? [],
+  };
 };
---
+++ src/types/joinRequest.type.ts
@@ -0,0 +1,54 @@
+import { UserProfileProps } from './accountType';
+
+export type JoinRequestStatus =
+  | 'PENDING'
+  | 'ACCEPTED'
+  | 'REJECTED'
+  | 'CANCELLED';
+
+export type DirectionRequest = 'FROM_USER' | 'FROM_ORGANIZATION';
+
+export interface IJoinRequestApiData {
+  // senderId: string | number;
+  receiverOrganizationId: string;
+  receiverUserId?: string;
+  direction?: DirectionRequest;
+  // status?: JoinRequestStatus;
+}
+
+export interface IJoinRequestResponse {
+  status: string;
+  code: string;
+  message: string;
+  data: {
+    joinRequests: IJoinRequests[];
+  };
+}
+
+export interface ICreateJoinRequestResponse {
+  status: string;
+  code: string;
+  message: string;
+  data: {
+    joinRequest: IJoinRequests; // 👈 один об'єкт
+  };
+}
+
+export interface IJoinRequests {
+  id: string;
+  status?: JoinRequestStatus;
+  direction: DirectionRequest;
+  sender?: ISender;
+  senderOrganization: ISenderOrg;
+}
+
+export interface ISender {
+  id: string;
+  email: string;
+  profile?: UserProfileProps;
+}
+
+export interface ISenderOrg {
+  id: string;
+  name: string;
+}
