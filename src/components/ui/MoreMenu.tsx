aec76f8e51493a2e9698faa866ef6b1525c90f42
Alona Kuznietsova <acvetochka@gmail.com>
2026-06-04 19:42:27 +0200

    create MoreMenu -UI component

---
+++ src/components/ui/MoreMenu.tsx
@@ -0,0 +1,72 @@
+import { JSX, ReactNode } from 'react';
+import { More } from '@/components/icons';
+import { cn } from '@/lib/utils';
+
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { stopEvent } from '@/lib/stopEvent';
+
+export type MoreMenuItem = {
+  id: string;
+  content: (close: () => void) => ReactNode;
+};
+
+type MoreMenuProps = {
+  items: MoreMenuItem[];
+  className?: string;
+  triggerClassName?: string;
+  menuWrapperClassName?: string;
+  side?: 'top' | 'right' | 'bottom' | 'left';
+  align?: 'start' | 'center' | 'end';
+};
+
+export const MoreMenu = ({
+  items,
+  className = '',
+  triggerClassName = '',
+  menuWrapperClassName = '',
+  side = 'bottom',
+  align = 'end',
+}: MoreMenuProps): JSX.Element => {
+  return (
+    <div className={cn('relative', className)}>
+      <DropdownMenu.Root modal={false}>
+        <DropdownMenu.Trigger asChild>
+          <button
+            type="button"
+            className={cn('px-2', triggerClassName)}
+            onPointerDown={(e) => e.stopPropagation()}
+            onClick={stopEvent}
+          >
+            <More className="cursor-pointer size-5 text-foreground hover:text-btn-hover active:text-btn-active" />
+          </button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Portal>
+          <DropdownMenu.Content
+            side={side}
+            align={align}
+            avoidCollisions
+            onClick={(e) => e.stopPropagation()}
+            className={cn(
+              'rounded-lg bg-admin-more p-4 shadow-lg translate-y-3',
+              menuWrapperClassName
+            )}
+          >
+            <ul className="flex flex-col gap-3">
+              {items.map((item) => (
+                <li key={item.id}>
+                  {item.content(() => {
+                    const event = new KeyboardEvent('keydown', {
+                      key: 'Escape',
+                    });
+
+                    document.dispatchEvent(event);
+                  })}
+                </li>
+              ))}
+            </ul>
+          </DropdownMenu.Content>
+        </DropdownMenu.Portal>
+      </DropdownMenu.Root>
+    </div>
+  );
+};
