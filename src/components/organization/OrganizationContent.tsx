4689352e3c091192f14d2bd6964fb21ac6632816
Alona Kuznietsova <acvetochka@gmail.com>
2026-08-02 15:38:21 +0200

    update OrganizatioContent

--- src/components/organization/OrganizationContent.tsx
+++ src/components/organization/OrganizationContent.tsx
@@ -1,50 +1,47 @@
 'use client';
 
-// import { getUserRole } from '@/lib/getUserRole';
 import { ContentProps, OrganizationDetailedProps } from '@/types';
 import { useTranslations } from 'next-intl';
 import { JSX } from 'react';
 import { OrgTasksSection } from '@/components/organization/tasksSection/OrgTasksSection';
 import { OrgMemberSection } from '@/components/organization/memberSection/OrgMemberSection';
-// import { OrgJoinRequestSection } from '@/components/organization/requestSection/OrgJoinRequestSection';
 import { OrgReviewSection } from '@/components/organization/reviewSection/OrgReviewSection';
 import { OrgMoreSection } from '@/components/organization/moreSection/OrgMoreSection';
 import { OrganizationContentPanel } from './OrganizationContentPanel';
 import { useOrganizationPermissions } from '@/hooks/useOrganizationPermissions';
-import { IJoinRequests } from '@/types/joinRequest.type';
-// import { OrgJoinRequestClient } from './requestSection/OrgJoinRequestClient';
 import { useUserRole } from '../providers/UserRoleProvider';
 import { OrgJoinRequestSection } from './requestSection/OrgJoinRequestSection';
+import { useJoinRequests } from '@/hooks/useJoinRequests';
 
 export const OrganizationContent = ({
   organization,
-  joinRequests,
 }: {
   organization: OrganizationDetailedProps;
-  joinRequests: IJoinRequests[];
 }): JSX.Element => {
   const t = useTranslations('organization');
   const views = t.raw('contentViews') as ContentProps[];
-  // const userRole = getUserRole(organization.members);
   const userRole = useUserRole();
   const { canViewRequests } = useOrganizationPermissions(userRole);
 
+  const { joinRequests, approve, reject } = useJoinRequests(organization.id);
+
   const userViews = views.filter((view) => view.id !== 'requests');
-  const visibleViews: ContentProps[] =
-    userRole === 'ADMIN' || userRole === 'MODERATOR' ? views : userViews;
+  const visibleViews: ContentProps[] = canViewRequests ? views : userViews;
 
   const viewComponents: Record<string, React.ReactNode> = {
     tasks: <OrgTasksSection tasks={organization.tasks} role={userRole} />,
     members: (
       <OrgMemberSection
         members={organization.members}
-        role={userRole}
         orgId={organization.id}
       />
     ),
     requests: canViewRequests && (
-      // <OrgJoinRequestSection orgId={organization.id} />
-      <OrgJoinRequestSection data={joinRequests} />
+      <OrgJoinRequestSection
+        onApprove={approve}
+        onReject={reject}
+        data={joinRequests}
+      />
     ),
     reviews: (
       <OrgReviewSection reviews={organization.reviews} role={userRole} />
--- src/components/organization/OrganizationLayout.tsx
+++ src/components/organization/OrganizationLayout.tsx
@@ -1,59 +1,28 @@
-// 'use client';
-
-// import { OrganizationDetailedProps } from '@/types';
-// import { JSX } from 'react';
-// import { Section } from '@/components/ui/Section';
-// import { OrganizationDesc } from '@/components/organization/OrganizationDesc';
-// import { OrganizationContent } from '@/components/organization/OrganizationContent';
-
-// export const OrganizationLayout = ({
-//   organization,
-// }: {
-//   organization: OrganizationDetailedProps;
-// }): JSX.Element => {
-//   return (
-//     <>
-//       <Section>
-//         <OrganizationDesc organization={organization} />
-//       </Section>
-//       <OrganizationContent organization={organization} />
-//     </>
-//   );
-// };
-
 'use client';
 
 import { OrganizationDetailedProps, Role } from '@/types';
-// import { Section } from 'lucide-react';
 import { OrganizationDesc } from './OrganizationDesc';
 import { OrganizationContent } from './OrganizationContent';
-import { IJoinRequests } from '@/types/joinRequest.type';
 import { JSX } from 'react';
 import { UserRoleProvider } from '../providers/UserRoleProvider';
 import { Section } from '../ui/Section';
 
 type OrgLayoutProps = {
   organization: OrganizationDetailedProps;
-  joinRequests: IJoinRequests[];
   userRole: Role;
 };
 
 export const OrganizationLayout = ({
   organization,
-  joinRequests,
   userRole,
 }: OrgLayoutProps): JSX.Element => {
   return (
     <UserRoleProvider role={userRole}>
       <Section>
         <OrganizationDesc organization={organization} />
-        {/* <div>TEST DESC</div> */}
       </Section>
 
-      <OrganizationContent
-        organization={organization}
-        joinRequests={joinRequests}
-      />
+      <OrganizationContent organization={organization} />
     </UserRoleProvider>
   );
 };
